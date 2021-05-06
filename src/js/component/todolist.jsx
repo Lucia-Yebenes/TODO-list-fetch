ea6177aa824be13a119a98cc2a98f856d64029d2
Lucia Yebenes <yebeneslucia@gmail.com>
2021-05-06 15:01:15 +0000

    Ej terminado

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import shortid from "shortid";
 
 const Todolist = () => {
+	let [homework, setHomework] = useState("");
+	const [task, setTask] = useState([]);
+	const thingsToDo = e => {
+		e.preventDefault();
+		console.log("hice enter");
+		setHomework("");
+		setTask([...task, { id: shortid.generate(), nameTask: homework }]);
+	};
+	const deleteTask = id => {
+		//console.log(id);
+		const arrayFiltrado = task.filter(item => item.id !== id);
+		setTask(arrayFiltrado);
+	};
 	return (
 		<>
-			<h1>todo</h1>
+			<div className="continer">
+				<h1 className="text-center">To do list</h1>
+				<form onSubmit={thingsToDo} className="col-5 m-auto">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Wath need to be done?"
+						onChange={e => {
+							setHomework(e.target.value);
+						}}
+						value={homework}
+					/>
+				</form>
+				<div className="todolist col-5 m-auto ">
+					<ul className="list-group">
+						{task.map(item => (
+							<li className="list-group-item" key={item.id}>
+								<span className="lead">{item.nameTask}</span>
+								<button
+									className="btn btn-sm float-right mx-2"
+									onClick={() => deleteTask(item.id)}>
+									X
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 };
